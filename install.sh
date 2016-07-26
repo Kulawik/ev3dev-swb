dabfe14cf14b9d85a9df10da9459dbf83f6ff886
Kulawik <Kulawik.repo@gmail.com>
2016-07-26 13:07:11 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,29 @@
+set -e
+
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 2B210565
+sudo apt-add-repository "deb http://archive.ev3dev.org/ubuntu trusty main"
+sudo apt-get update
+sudo apt-get install brickstrap
+
+# create a supermin appliance
+sudo update-guestfs-appliance
+# add yourself to the kvm group
+# need to log out and back in for this to take effect
+sudo usermod -a -G kvm $USER
+newgrp kvm
+# fix permissions on /boot/vmlinuz*
+sudo chmod +r /boot/vmlinuz*
+# And you need to add yourself to /etc/subuid and /etc/subgid to be able to use uid/gid mapping.
+sudo usermod --add-subuids 200000-265534 --add-subgids 200000-265534 $USER
+
+# create virtual environment
+mkdir brickstrap
+cd brickstrap
+# -p argument from ls /usr/share/brickstrap/projects
+# -c argument form ls /usr/share/brickstrap/projects/ev3dev-jessie
+brickstrap -p ev3dev-jessie -c ev3 -d ev3-rootfs create-rootfs
+brickstrap -d ev3-rootfs shell << EOF
+apt-get update
+apt-get install build-essential
+EOF
+
