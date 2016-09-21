5e2ed3cd08ab083cd3568d66857f983f8aad8144
Kulawik <Kulawik.repo@gmail.com>
2016-09-21 14:40:42 +0200

    add draft.cpp

---
+++ lego_fw/src/draft.cpp
@@ -0,0 +1,40 @@
+
+
+void go_up_action(CraneControl crane, CraneData data) {
+	crane.vMotor().set_position_sp(100);
+	crane.vMotor().set_command("run-to-abs-pos");
+}
+
+void go_down_action(CraneControl crane, CraneData data) {
+	CraneControl::motor_ptr motor = crane.vMotor();
+	motor->set_position_sp(0);
+	motor->set_command("run-to-abs-pos");
+}
+
+/*
+void go_left_action(CraneControl crane, CraneData data) {
+	crane.hMotor().set_position_sp(90);
+	crane.hMotor().set_command("run-to-abs-pos");
+}
+*/
+
+bool condition_A_to_B(CraneData data, ElapsedTime time_in_state) {
+	return data.v_motor_position == 100 || data.v_motor_speed == 0;
+}
+
+
+int main() {
+
+	State state_A, state_B, state_C;
+
+	state_A.addActions(go_up_action);
+	state_A.addTransition(condition_A_to_B, state_B); // (condition callback, target state) 
+
+	state_B.addAction(go_down_action);
+	//state_B.addAction(go_left_action);
+	//state_B.addTransition([](CraneData, ElapsedTime)->bool{return true;}, state_C);
+
+	state_A.visit();
+	
+	return 0;
+}
