c4dd2739f226321dc5fb789d24121c26f9bc6e69
Kulawik <Kulawik.repo@gmail.com>
2016-11-08 14:41:53 +0100

    Add State api

--- lego_fw/src/State.hpp
+++ lego_fw/src/State.hpp
@@ -1,15 +1,29 @@
 #ifndef STATE_HPP 
 #define STATE_HPP 
 
+#include <chrono>
+
+typedef std::chrono::milliseconds Time;
+
 template<typename Data, typename Control>
 class State {
    public:
+	typedef std::function<void(CraneControl& crane, CraneData& data)> ActionCallback;
+	typedef std::function<void(CraneData& data, const Time& elapsed_time)> ConditionCallback;
+	typedef std::pair<ConditionCallback, State*> Transition;
     State() = default;
     State<Control>* next(const Data& d);
     State<Control>& enter();
-
+	
+	void addAction(const ActionCallback& action) {
+		actions_.push_back(action);
+	}
+	void addTransition(const ConditionCallback& condition, State* next_state) {
+		transitions_.emplace_back(std::make_pair(condition, next_state));
+	}
    private:
-    std::vector<std::function<bool(const Data&, Time > 
+    std::vector<ActionCallback> actions_;
+	std::vector<Transition> transitions_; 
 };
 
 #endif
