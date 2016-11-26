7b2b9e6d5973f578ab7729924d58fed8d4b46566
Kulawik <Kulawik.repo@gmail.com>
2016-11-26 20:25:10 +0100

    Implement State::enter, add tests in test_state

--- lego_fw/src/State.hpp
+++ lego_fw/src/State.hpp
@@ -12,23 +12,25 @@ typedef std::chrono::time_point<std::chrono::system_clock> TimePoint;
 template<typename Data, typename Control>
 class State {
    public:
-    typedef std::function<void(Control& control, const Data& data)>
+    typedef typename std::function<void(Control& control, const Data& data)>
         ActionCallback;
-    typedef std::function<void(const Data& data, const Time& elapsed_time)>
+    typedef typename std::function<bool(const Data& data, const Time& elapsed_time)>
         ConditionCallback;
-    typedef std::pair<ConditionCallback, State<Data, Control>*> Transition;
-    typedef std::vector<ActionCallback> Actions;
-    typedef std::vector<Transition> Transitions;
+    typedef typename std::pair<ConditionCallback, State<Data, Control>*> Transition;
+    typedef typename std::vector<ActionCallback> Actions;
+    typedef typename std::vector<Transition> Transitions;
 
-    State() = default;
-    ~State() = default;
+    State() : timeout_(nullptr) {}
+    ~State() { delete timeout_; }
 
     /** Check if one of transitions' conditions is satisfied,
      *  if so returns next state of first applicable transition,
      *  otherwise returns "this" (the same state)
      */
     State<Data, Control>* next(const Data& data) {
-        Time elapsed_time = std::chrono::system_clock::now() - time_entered_;
+        const TimePoint now = std::chrono::system_clock::now();
+        Time elapsed_time =
+            std::chrono::duration_cast<Time>(now - time_entered_);
         for (auto& transition : transitions_) {
             if (transition.first(data, elapsed_time)) {
                 return transition.second;
@@ -37,10 +39,15 @@ class State {
         return this;
     }
 
-    /** Remember time of entrance to the state */
-    State<Data, Control>& enter() {
+    /** Save time of entrence and perform state's actions
+     */
+    void enter(Control& control, const Data& data) {
+        /** save time of entrance to the state */
         time_entered_ = std::chrono::system_clock::now();
-        //TODO
+        /** perform all actions */
+        for (auto& action : actions_) {
+            action(control, data);
+        }
     }
 
     void addAction(const ActionCallback& action) {
@@ -52,12 +59,24 @@ class State {
         transitions_.emplace_back(std::make_pair(condition, next_state));
     }
 
+    /** return pointer to timeout, if none set return nullptr
+     */
+    Time* getTimeout() {
+        return timeout_;
+    }
+
+    void setTimeout(const Time& timeout) {
+        delete timeout_;
+        timeout_ = new Time(timeout);
+    }
+
    private:
     State(const State&) = delete;
 
     Actions actions_;
     Transitions transitions_;
     TimePoint time_entered_;
+    Time* timeout_;
 };
 
 #endif
--- lego_fw/test/test_state.cpp
+++ lego_fw/test/test_state.cpp
@@ -3,6 +3,9 @@
 #include <boost/test/unit_test.hpp>
 
 #include <vector>
+#include <chrono>
+#include <algorithm>
+#include <thread>
 
 #define private public
 #include "../src/State.hpp"
@@ -11,25 +14,41 @@
 
 namespace {
 
+struct XControl {
+    XControl() : x_(0) {}
+    int getX() {
+        return x_;
+    }
+    void incX() {
+        ++x_;
+    }
+   private:
+    int x_;
+};
+
 struct XData {
     int x;
+    XData(XControl& control) : control_(control) {}
     bool update() {
-        ++x;
+        x = control_.getX();
         return true;
     }
+   private:
+        XControl& control_;
 };
+
 
 } // namespace
 
 BOOST_AUTO_TEST_SUITE(test_state)
 
-/** Test if State::addAction adds actions to actions container */
+/** Test if State::addAction adds actions to actions' container */
 BOOST_AUTO_TEST_CASE( test_addAction ) {
     typedef XData Data;
-    typedef int Control;
+    typedef XControl Control;
     State<Data, Control> state;
     BOOST_CHECK_EQUAL(state.actions_.size(), 0);
-    auto action = [](Control& c, const Data& d) {};
+    auto action = [](Control&, const Data&) {};
     state.addAction(action);
     BOOST_CHECK_EQUAL(state.actions_.size(), 1);
     state.addAction(action);
@@ -38,6 +57,119 @@ BOOST_AUTO_TEST_CASE( test_addAction ) {
         state.addAction(action);
     }
     BOOST_CHECK_EQUAL(state.actions_.size(), 10);
+}
+
+/** Test if State::addTransition adds transitions to transitions' container */
+BOOST_AUTO_TEST_CASE( test_addTransition ) {
+    typedef XData Data;
+    typedef XControl Control;
+    State<Data, Control> state;
+    BOOST_CHECK_EQUAL(state.transitions_.size(), 0);
+    auto condition = [](const Data&, const Time&) { return true; };
+    state.addTransition(condition, nullptr);
+    BOOST_CHECK_EQUAL(state.transitions_.size(), 1);
+    state.addTransition(condition, nullptr);
+    BOOST_CHECK_EQUAL(state.transitions_.size(), 2);
+    for (int i=2; i<10; ++i) {
+        state.addTransition(condition, nullptr);
+    }
+    BOOST_CHECK_EQUAL(state.transitions_.size(), 10);
+}
+
+BOOST_AUTO_TEST_CASE( test_get_set_Timeout ) {
+    typedef XData Data;
+    typedef XControl Control;
+    State<Data, Control> state;
+    BOOST_CHECK(state.getTimeout() == nullptr);
+    state.setTimeout(Time(1));
+    BOOST_REQUIRE(state.timeout_ != nullptr);
+    BOOST_CHECK(*state.timeout_ == Time(1));
+    Time* timeout = state.getTimeout();
+    BOOST_REQUIRE(timeout != nullptr);
+    BOOST_CHECK(*timeout == Time(1));
+    timeout = state.getTimeout();
+    BOOST_REQUIRE(timeout != nullptr);
+    BOOST_CHECK(*timeout == Time(1));
+    state.setTimeout(Time(2));
+    timeout = state.getTimeout();
+    BOOST_REQUIRE(timeout != nullptr);
+    BOOST_CHECK(*timeout == Time(2));
+}
+
+BOOST_AUTO_TEST_CASE( test_enter ) {
+    typedef XData Data;
+    typedef XControl Control;
+    State<Data, Control> state;
+    Control control;
+    Data data(control);
+    auto enter_time = std::chrono::system_clock::now();
+    state.enter(control, data);
+    BOOST_CHECK(state.time_entered_ - enter_time < std::chrono::milliseconds(1));
+    auto action = [](Control& c, const Data& d) {
+        if (d.x < 4) {
+            c.incX();
+        }
+    };
+    state.addAction(action);
+    state.enter(control, data);
+    enter_time = std::chrono::system_clock::now();
+    BOOST_CHECK(state.time_entered_ - enter_time < std::chrono::milliseconds(1));
+    BOOST_CHECK_EQUAL(control.getX(), 1);
+    data.update();
+    state.addAction(action);
+    state.addAction(action);
+    enter_time = std::chrono::system_clock::now();
+    state.enter(control, data);
+    BOOST_CHECK(state.time_entered_ - enter_time < std::chrono::milliseconds(1));
+    BOOST_CHECK_EQUAL(control.getX(), 4);
+    data.update();
+    state.enter(control, data);
+    BOOST_CHECK_EQUAL(control.getX(), 4);
+}
+
+BOOST_AUTO_TEST_CASE( test_next ) {
+    typedef XData Data;
+    typedef XControl Control;
+    State<Data, Control> state, A, B, C, T;
+    Control control;
+    Data data(control);
+    BOOST_CHECK(state.next(data) == &state);
+    auto cond_A = [](const Data& d, const Time&) {return d.x == 1;};
+    auto cond_B = [](const Data& d, const Time&) {return d.x == 2;};
+    auto cond_C = [](const Data& d, const Time&) {return d.x == 3;};
+    auto cond_X = [](const Data& d, const Time&) {return d.x > 0;};
+
+    state.addTransition(cond_A, &A);
+    data.x = 0;
+    BOOST_CHECK(state.next(data) == &state);
+    data.x = 1;
+    BOOST_CHECK(state.next(data) == &A);
+
+    state.addTransition(cond_B, &B);
+    state.addTransition(cond_C, &C);
+    state.addTransition(cond_X, nullptr);
+
+    data.x = 0;
+    BOOST_CHECK(state.next(data) == &state);
+    data.x = 1;
+    BOOST_CHECK(state.next(data) == &A);
+    data.x = 2;
+    BOOST_CHECK(state.next(data) == &B);
+    data.x = 3;
+    BOOST_CHECK(state.next(data) == &C);
+    data.x = 4;
+    BOOST_CHECK(state.next(data) == nullptr);
+
+    data.x = -1;
+    BOOST_CHECK(state.next(data) == &state);
+    auto cond_T = [](const Data&, const Time& t) {
+        return t > std::chrono::milliseconds(9);
+    };
+    state.enter(control, data);
+    state.addTransition(cond_T, &T);
+    BOOST_CHECK(state.next(data) == &state);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    BOOST_CHECK(state.next(data) == &T);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
