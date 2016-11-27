c463739d7eb24a41c1f80745f59e9729f857c918
Łukasz Neumann <fuine@riseup.net>
2016-11-27 16:53:23 +0100

    Fix Safe constructor

--- lego_fw/src/Safe.hpp
+++ lego_fw/src/Safe.hpp
@@ -14,7 +14,7 @@ public:
     // c-tor. User should pass both data reference as well as the mutex
     Safe(std::mutex& mtx, T& dv) : lock_(mtx), device_(dv) { }
     // move c-tor
-    explicit Safe(Safe<T>&& s) : lock_(std::move(s.lock_)), device_(s.device_) { }
+    Safe(Safe<T>&& s) : lock_(std::move(s.lock_)), device_(s.device_) { }
     // operator overloads
     T operator*() const { return device_; }
     T* operator->() const { return &device_; }
